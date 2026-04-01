96503ce286e6713657cad764b41445acebceee86
alberto-debug <albertojunior848423803@gmail.com>
2026-04-01 12:01:45 +0300

    TokenService for token validation and expiration

--- user-service /user-service /src/main/java/com/alberto/multidigitalwallet/user_service/security/TokenService.java
+++ user-service /user-service /src/main/java/com/alberto/multidigitalwallet/user_service/security/TokenService.java
@@ -1,6 +1,7 @@
 package com.alberto.multidigitalwallet.user_service.security;
 
 import com.alberto.multidigitalwallet.user_service.exception.TokenGenerationException;
+import com.alberto.multidigitalwallet.user_service.exception.TokenValidationException;
 import com.alberto.multidigitalwallet.user_service.model.entity.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -41,6 +42,33 @@ public class TokenService {
         } catch (JWTCreationException e) {
             log.error("Token Creation Failed", e);
             throw new TokenGenerationException("Failed to generate token ", e);
+        }
+
+    }
+
+    public String validateToken( String token){
+        try{
+
+            if (token == null || token.isEmpty()){
+                throw new TokenValidationException("Token cannot be null or empty");
+            }
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer(issuer)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+
+
+        } catch (JWTCreationException e) {
+            log.warn("JWT verification failed: {}", e.getMessage());
+            throw new TokenValidationException("Invalid or expired token", e);
+        } catch (Exception e) {
+            log.error("Unexpected error during token validation", e);
+            throw new TokenValidationException("Token validation failed", e);
         }
 
     }
