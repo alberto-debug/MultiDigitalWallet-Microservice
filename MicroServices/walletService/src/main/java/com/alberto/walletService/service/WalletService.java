e5ef30c2f60352e1ed13717a733003f976f291d9
alberto-debug <albertojunior848423803@gmail.com>
2026-05-10 13:44:56 +0300

    Addition of Exceptions, createWallet Service and wallet mapper

---
+++ MicroServices/walletService/src/main/java/com/alberto/walletService/service/WalletService.java
@@ -0,0 +1,47 @@
+package com.alberto.walletService.service;
+
+
+import com.alberto.walletService.DTOs.CreateWalletRequest;
+import com.alberto.walletService.DTOs.WalletResponse;
+import com.alberto.walletService.exception.WalletAlreadyExistsException;
+import com.alberto.walletService.mapper.WalletMapper;
+import com.alberto.walletService.model.ENUMs.WalletStatus;
+import com.alberto.walletService.model.Wallet;
+import com.alberto.walletService.repository.TransactionRepository;
+import com.alberto.walletService.repository.WalletRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class WalletService {
+
+    private final TransactionRepository transactionRepository;
+    private final WalletRepository walletRepository;
+    private final WalletMapper walletMapper;
+
+    public WalletResponse createWallet(UUID userId, CreateWalletRequest request){
+
+        boolean alreadyExists = walletRepository.existsByUserIdAndCurrency(userId, request.currency());
+
+        if (alreadyExists){
+            throw new WalletAlreadyExistsException(
+                    "User already has a " + request.currency() + " wallet"
+            );
+        }
+
+
+        Wallet wallet = new Wallet();
+        wallet.setUserId(userId);
+        wallet.setCurrency(request.currency());
+        wallet.setBalance(BigDecimal.ZERO);
+        wallet.setWalletStatus(WalletStatus.ACTIVE);
+
+        Wallet saved = walletRepository.save(wallet);
+        
+        return walletMapper.toResponse(saved);
+    }
+}
